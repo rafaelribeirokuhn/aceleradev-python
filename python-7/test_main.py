b69c44275e25eec97b4754735909f17744d8c96e
Rafael Ribeiro Kühn <rafaelribeirokuhn@gmail.com>
2020-06-01 00:53:05 -0300

    solved challenge python-7

--- python-7/test_main.py
+++ python-7/test_main.py
@@ -1,5 +1,18 @@
 from main import get_temperature
+from unittest import mock
 
 
 def test_get_temperature_by_lat_lng():
-    raise NotImplementedError
+    lat = -14.235004
+    lng = -51.92528
+
+    with mock.patch('main.requests.get') as mock_get:
+        mock_get.return_value.json.return_value = {
+            "currently": {
+                "temperature": 62
+            }
+        }
+
+        celsius_degree = get_temperature(lat, lng)
+
+    assert celsius_degree == 16

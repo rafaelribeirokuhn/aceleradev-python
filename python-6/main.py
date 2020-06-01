b3a2ffe33ca9f241810ef418c45d0bffba047db5
Rafael Ribeiro Kühn <rafaelribeirokuhn@gmail.com>
2020-06-01 00:48:46 -0300

    solved challenge python-6

--- python-6/main.py
+++ python-6/main.py
@@ -8,31 +8,64 @@ class Department:
 
 class Employee:
     def __init__(self, code, name, salary):
+        if type(self) == Employee:
+            raise TypeError('base class may not be instantiated')
+
         self.code = code
         self.name = name
         self.salary = salary
 
     def calc_bonus(self):
-        pass
+        raise NotImplementedError
 
     def get_hours(self):
-        pass
+        raise NotImplementedError
+
+    def get_department(self):
+        raise NotImplementedError
+
+    def set_department(self, department):
+        raise NotImplementedError
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('managers', 1)
+        self.__department = Department('managers', 1)
 
     def calc_bonus(self):
         return self.salary * 0.15
 
+    def get_hours(self):
+        return 8
 
-class Seller(Manager):
+    def get_department(self):
+        return self.__department.name
+
+    def set_department(self, department):
+        self.__department = department
+
+
+class Seller(Employee):
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
-        self.departament = Department('sellers', 2)
-        self.sales = 0
+        self.__department = Department('sellers', 2)
+        self.__sales = 0
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
 
     def get_hours(self):
-        return 6
+        return 8
+
+    def get_department(self):
+        return self.__department.name
+
+    def set_department(self, department):
+        self.__department = department
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sale):
+        self.__sales += sale

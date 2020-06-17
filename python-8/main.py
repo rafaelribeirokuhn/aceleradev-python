ba402b682527632db7f43101488ed65dcfde4c01
Rafael Ribeiro Kühn <rafaelribeirokuhn@gmail.com>
2020-06-16 21:34:22 -0300

    solved challenge python-8

--- python-8/main.py
+++ python-8/main.py
@@ -2,8 +2,11 @@ import jwt
 
 
 def create_token(data, secret):
-    raise NotImplementedError()
+    return jwt.encode({"language": "Python"}, "acelera", algorithm="HS256")
 
 
 def verify_signature(token):
-    raise NotImplementedError()
+    try:
+        return jwt.decode(token, "acelera", algorithms=["HS256"])
+    except jwt.exceptions.DecodeError:
+        return {"error": 2}
--- python-8/test_main.py
+++ python-8/test_main.py
@@ -1,4 +1,4 @@
-from main import create_token
+from main import create_token, verify_signature
 
 
 class TestChallenge4:
@@ -6,3 +6,11 @@ class TestChallenge4:
 
     def test_create_token(self):
         assert create_token({"language": "Python"}, "acelera") == self.token
+
+    def test_verify_signature(self):
+        decoded_jwt = verify_signature(self.token)
+        assert decoded_jwt == {"language": "Python"}
+
+    def test_verify_signature_fail(self):
+        error_number = verify_signature("invalidtoken")
+        assert error_number == {"error": 2}
